26babb64cf49e73fd7d0615a686b742a0ddabedc
timka <timkaa555@gmail.com>
2023-11-09 04:19:09 +0300

    front

---
+++ front/front/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import {useState} from "react";
+
+function App() {
+    const [message, setMessage] = useState('');
+
+    const handleTextChange = (event) => {
+        setMessage(event.target.value);
+    };
+
+    function handleSendButton() {
+        const queryParam = JSON.stringify({message})
+        console.log(queryParam)
+        fetch('http://localhost:8080/article', {
+        
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            // body: JSON.stringify({text}),
+            body: queryParam
+        })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Error:', error));
+    }
+
+    return (
+        <div>
+            <textarea value={message} onChange={handleTextChange} />
+            <button onClick={handleSendButton}>
+            </button>
+        </div>
+    );
+}
+
+export default App;
